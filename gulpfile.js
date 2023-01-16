9ef7f79a2c3485ca9e767079243c7c58789acf07
ynanxu <ynanxu@trip.com>
2023-01-16 16:18:58 +0800

    add index page

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 const gulp = require("gulp"); // 引入 gulp
 const path = require("path");
-gulp.task("default", () => {
+gulp.task("love", () => {
   return gulp.src("./src/love/**/*").pipe(gulp.dest("./dist/love"));
 });
+gulp.task("firstpage", () => {
+  return gulp.src("./src/README.md").pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("default", gulp.series("love", "firstpage"));
